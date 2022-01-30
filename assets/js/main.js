3147ab59b3bd7da83d542fa26ca484a1169d9754
Sabin Baniya <dummymail@tutanota.de>
2022-01-30 15:28:26 +0545

    Hero added

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,14 +10,14 @@ const overlay = document.getElementById("overlay");
 icon.addEventListener("click", () => {
   menu.classList.toggle("hidden");
   overlay.classList.toggle("hidden");
-  menu.classList.toggle("absolute");
-  menu.classList.toggle("p-4");
-  menu.classList.toggle("bg-gray-50");
-  menu.classList.toggle("top-1/2");
-  menu.classList.toggle("left-1/2");
-  menu.classList.toggle("-translate-x-1/2");
-  menu.classList.toggle("translate-y-1/2");
-  menu.classList.toggle("text-center");
+  // menu.classList.toggle("absolute");
+  // menu.classList.toggle("p-4");
+  // menu.classList.toggle("bg-gray-50");
+  // menu.classList.toggle("top-1/2");
+  // menu.classList.toggle("left-1/2");
+  // menu.classList.toggle("-translate-x-1/2");
+  // menu.classList.toggle("translate-y-1/2");
+  // menu.classList.toggle("text-center");
 });
 
 /* Testimonial Scroll start */
